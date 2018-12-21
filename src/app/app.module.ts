0e3874e42c8d0f85b8ac363d772ac5e34e63fbac
Imene Khemiri <khemiriimene@gmail.com>
2018-12-21 13:11:49 +0100

    Edit the app.moduke.ts to inject the PageNotFound component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {AppRoutingModule} from '.app-routing.module'
+import {AppRoutingModule} from './app-routing.module'
 import { MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule, MatToolbarModule } from '@angular/material';
 import { from } from 'rxjs';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
