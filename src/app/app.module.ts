c6776c7ed2a9f3628c0dced04f5f6d045ff3bb3d
Imene Khemiri <khemiriimene@gmail.com>
2018-12-21 12:49:45 +0100

    Edit ./src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {AppRoutingModule} from '.app-routing.module'
+import { MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule, MatToolbarModule } from '@angular/material';
+import { from } from 'rxjs';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialModule,
+    MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule, MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
