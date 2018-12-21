6be93973d51d3bf2b801ee519692cad632863851
Imene Khemiri <khemiriimene@gmail.com>
2018-12-21 15:25:29 +0100

    Add the navigation compoenet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { OperationsComponent} from './operations/operations.component';
+import { OperationsHistoryComponent } from './operations-history/operations-history.component';
+
 
 export const routes: Routes = [
+    { path: 'operationshistory',component: OperationsHistoryComponent},
     { path: 'operations', component: OperationsComponent},
     { path: '404', component: PageNotFoundComponent },
     { path: '**', redirectTo: '404' }
