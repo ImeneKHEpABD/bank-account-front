54cb813c85c14cb3bb5868130626a262b27cae5a
Imene Khemiri <khemiriimene@gmail.com>
2019-01-04 00:13:36 +0100

    Add the custom css files to the project

--- src/app/newaccount/newaccount.component.spec.ts
+++ src/app/newaccount/newaccount.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NewaccountComponent } from './newaccount.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule, MatToolbarModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '../material.module';
 
 describe('NewaccountComponent', () => {
   let component: NewaccountComponent;
@@ -8,6 +12,11 @@ describe('NewaccountComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        MaterialModule,
+        NoopAnimationsModule,
+        FormsModule, ReactiveFormsModule
+      ],
       declarations: [ NewaccountComponent ]
     })
     .compileComponents();
