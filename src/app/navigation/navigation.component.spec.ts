5de11b2e5e1ee442e0cba616728a2cbbea24a9d0
Imene Khemiri <khemiriimene@gmail.com>
2019-01-08 17:52:04 +0100

    Add the tests related to the message display when entering a negative amount in the operations component

--- src/app/navigation/navigation.component.spec.ts
+++ src/app/navigation/navigation.component.spec.ts
@@ -1,46 +1,49 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 
 import { NavigationComponent } from './navigation.component';
 import { AppModule } from '../app.module';
 import { AppRoutingModule } from '../app-routing.module';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
+import { Location } from "@angular/common"
 
-@Component({selector: 'router-outlet', template: ''})
+@Component({ selector: 'router-outlet', template: '' })
 class RouterOutletStubComponent { }
 
 describe('NavigationComponent', () => {
   let component: NavigationComponent;
   let fixture: ComponentFixture<NavigationComponent>;
-
+  let router:Router;
+  let location:Location;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         AppModule,
         RouterTestingModule.withRoutes([])
-     ]
-      
-     
+      ]
+
+
     })
-    .overrideModule(AppModule, {
-      remove: {
+      .overrideModule(AppModule, {
+        remove: {
           imports: [
             AppRoutingModule
           ]
-      },
-      add: {
+        },
+        add: {
           declarations: [
             NavigationComponent,
             RouterOutletStubComponent
           ]
-      }
-  })
-    .compileComponents()
-    .then(() => {
-      fixture = TestBed.createComponent(NavigationComponent);
-      component    = fixture.componentInstance;
-      fixture.detectChanges();
-  });
+        }
+      })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(NavigationComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+      });
   }));
 
   beforeEach(() => {
@@ -52,4 +55,6 @@ describe('NavigationComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+ 
 });
--- src/app/operations/operations.component.spec.ts
+++ src/app/operations/operations.component.spec.ts
@@ -30,15 +30,23 @@ describe('OperationsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  it('should reject a negative deposit amount', () => {
+  it('should reject a negative deposit amount and set the value to zero', () => {
     fixture.componentInstance.onDeposit(-100);
     expect(fixture.componentInstance.value).toBeGreaterThanOrEqual(0);
    
   });
-  it('should reject a negative withdraw amount', () => {
+  it('should reject a negative withdraw amount and set the value to zero', () => {
     fixture.componentInstance.onWithdraw(-100);
     expect(fixture.componentInstance.value).toBeGreaterThanOrEqual(0);
    
+  });
+  it('should display a message with `The amount should not be negative!!` when a negative withdraw amount is entreded', () => {
+    fixture.componentInstance.onWithdraw(-100);
+    expect(component.message).toMatch('The amount should not be negative!!');   
+  });
+  it('should display a message with `The amount should not be negative!!` when a negative deposit amount is entreded', () => {
+    fixture.componentInstance.onDeposit(-100);
+    expect(component.message).toMatch('The amount should not be negative!!');   
   });
   
 });
