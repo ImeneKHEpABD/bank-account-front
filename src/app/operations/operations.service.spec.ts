074ec006b1cd651ab1cc2d7e786eed20a4df9449
Imene Khemiri <khemiriimene@gmail.com>
2019-01-09 13:14:22 +0100

    Add the operations service and model

---
+++ src/app/operations/operations.service.spec.ts
@@ -0,0 +1,7 @@
+import { Operations } from './operations.service';
+
+describe('Operations', () => {
+  it('should create an instance', () => {
+    expect(new Operations()).toBeTruthy();
+  });
+});
