6bb18c810bd8b62549ffe42ea5dea23ff8ab1b08
Imene Khemiri <khemiriimene@gmail.com>
2019-01-09 14:20:59 +0100

    Update the operations service ts file and add the Withdraw and the DepositAmount methods

--- src/app/operations/operations.service.ts
+++ src/app/operations/operations.service.ts
@@ -1,2 +1,22 @@
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable()
 export class Operations {
+    value = 0;
+    private url = '/api/unsecure/operations';
+    constructor(private http: HttpClient) { }
+
+    WithdrawAmount(amount: number) {
+        this.value -= amount;
+    }
+
+    DepositAmount(amount: number) {
+        this.value += amount;
+    }
 }
