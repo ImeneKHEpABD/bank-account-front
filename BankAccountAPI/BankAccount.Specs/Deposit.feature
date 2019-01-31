5da297c7dcc32c20b6761c6cc5fcc34106c508b3
Imene Khemiri <khemiriimene@gmail.com>
2019-01-31 01:15:14 +0100

    Add the nunit, nfluent, testadapter to the bankAccountAPI project

--- BankAccountAPI/BankAccount.Specs/Deposit.feature
+++ BankAccountAPI/BankAccount.Specs/Deposit.feature
@@ -2,9 +2,12 @@ Feature: Deposit
   In order to save money
   As a bank client
   I want to make a deposit in my account
+
 @mytag
-Scenario: Negative amount should be rejected
+Scenario: Negative deposit amount should be rejected
 	Given a client has an account
 	And he wants to deposit an amount of -100
 	When he does deposit
-	Then he should be told " :The amount should not be negative!!"
+	Then he should be told " : Deposit amount should be strictly positive!!"
+
+
---
+++ BankAccountAPI/BankAccount.Specs/DepositSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace BankAccount.Specs
+{
+    [Binding]
+    public class DepositSteps
+    {
+        [Given(@"a client has an account")]
+        public void GivenAClientHasAnAccount()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"he wants to deposit an amount of (.*)")]
+        public void GivenHeWantsToDepositAnAmountOf(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"he does deposit")]
+        public void WhenHeDoesDeposit()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"he should be told ""(.*)""")]
+        public void ThenHeShouldBeTold(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
